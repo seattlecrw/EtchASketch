e21f0b3053e5cf1ca1e55e8733060c7472498e0c
Courtney Wilkins <seattlecrw@yahoo.com>
2016-08-01 15:21:06 -0700

    add color changing feature upon hover

--- script.js
+++ script.js
@@ -13,4 +13,21 @@ $(document).ready(function() {
 		$('.box').height((550 / num_squares) - 2);
 	}
 	render(num_squares);
+
+	// Create random color generator
+	var random_color_generate = function() {
+		var max_hex = parseInt('FFFFFF', 16);
+		var random_choice = Math.floor(Math.random() * max_hex);
+		var hexString = random_choice.toString(16);
+		return '#' + hexString;
+	};
+
+	// Create hover effect to change box colors
+	$('.box').hover(function() {
+		var new_color = random_color_generate();
+		$(this).css('background-color', new_color);
+		$(this).css('border', '1px solid #FFFFFF')
+    }, function() {
+    	$(this).css('border', '1px solid #000000')
+    });
 });
